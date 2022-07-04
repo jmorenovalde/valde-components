83be716547cabb8d316025314eb86a93d3859f7b
Juan Antonio Moreno Valderrama <jamv999@gmailcom>
2022-07-04 20:32:04 +0200

    add all butons to buttons page and styles to menu

--- src/app/pages/buttons/buttons.component.ts
+++ src/app/pages/buttons/buttons.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import {
+  ButtonSize,
+  ButtonStyle,
+} from '@shared/components/valde-button/valde-button.component';
 
 @Component({
   selector: 'app-buttons',
   templateUrl: './buttons.component.html',
 })
-export class ButtonsComponent {}
+export class ButtonsComponent {
+  public ButtonStyle = ButtonStyle;
+  public ButtonSize = ButtonSize;
+}
