39abf97b3bd8a33f4c2c33dee1b0d6865900282c
Juan Antonio Moreno Valderrama <jamv999@gmail.com>
2022-07-13 23:10:59 +0200

    add keyup ESC to restore value

--- src/app/shared/components/valde-form-field/valde-form-field.component.ts
+++ src/app/shared/components/valde-form-field/valde-form-field.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-valde-form-field',
   templateUrl: './valde-form-field.component.html',
   styleUrls: ['./valde-form-field.component.scss'],
 })
-export class ValdeFormFieldComponent {
+export class ValdeFormFieldComponent implements OnChanges {
   /**
    * This property indicate if the field is disabled or not.
    */
@@ -15,22 +15,40 @@ export class ValdeFormFieldComponent {
    * The identificator of the field
    * @required
    */
-  @Input() id = '';
+  @Input() id!: string;
   /**
    * The identification text of the field
    */
   @Input() label = 'Label';
 
   /**
+   * This input is only used if the component is type number, that indicates the min value.
+   */
+  @Input() min = Number.MIN_SAFE_INTEGER;
+
+  /**
+   * This input is only used if the component is type number, that indicates the maximun value.
+   */
+  @Input() max = Number.MAX_SAFE_INTEGER;
+
+  /**
    * Text to show when the field is empty.
    */
-  @Input() placeholder = '';
+  @Input() placeholder!: string;
 
   /**
    * This property indicate if the field is readonly or not.
    */
   @Input() readonly = false;
 
+  /**
+   * This input is only used if the component is type number, that indicates the step of the spin.
+   */
+  @Input() step = 1;
+
+  /**
+   * The type of the input.
+   */
   @Input() type:
     | 'text'
     | 'number'
@@ -45,8 +63,57 @@ export class ValdeFormFieldComponent {
     | 'url'
     | 'datetime-local' = 'text';
 
-  /**
-   * This input is only used if the component is type number, that indicates te step of the spin.
-   */
-  @Input() step = 1;
+  @Input() value!: unknown;
+
+  private isValid = true;
+  public valueToShow: any = '';
+  public valueOld: any = '';
+  public valueTemporal: any = '';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const { value } = changes;
+
+    if (value?.currentValue) {
+      this.valueToShow = value.currentValue;
+      this.valueOld = value.currentValue;
+    }
+  }
+
+  onKeyupNumbers(event: KeyboardEvent): void {
+    if (this.isEscPressed(event)) {
+      this.restoreValues();
+    } else {
+      const target = event.target as HTMLInputElement;
+      this.isValid = target?.validity?.valid;
+      if (this.isValid) {
+        this.valueTemporal = target.valueAsNumber;
+        this.valueToShow = target.valueAsNumber;
+      }
+    }
+  }
+
+  onKeyup(event: KeyboardEvent): void {
+    if (this.isEscPressed(event)) {
+      this.restoreValues();
+    } else {
+      const target = event.target as HTMLInputElement;
+      this.isValid = true;
+      this.valueTemporal = target.value;
+      this.valueToShow = target.value;
+    }
+    console.warn({
+      temporal: this.valueTemporal,
+      old: this.valueOld,
+      toShow: this.valueToShow,
+    });
+  }
+
+  private isEscPressed(event: KeyboardEvent): boolean {
+    return event?.code === 'Escape';
+  }
+
+  private restoreValues() {
+    this.valueToShow = this.valueOld;
+    this.valueTemporal = '';
+  }
 }
--- src/app/shared/components/valde-form-field/valde-form-field.module.ts
+++ src/app/shared/components/valde-form-field/valde-form-field.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ValdeFormFieldComponent } from './valde-form-field.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ValdeFormFieldComponent],
   exports: [ValdeFormFieldComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
 })
 export class ValdeFormFieldModule {}
